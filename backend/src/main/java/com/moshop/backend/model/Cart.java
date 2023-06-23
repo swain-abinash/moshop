c27f81a22a6ee32bf0321b68adc05c4f40d929d5
Dipasikha Sahoo <dipasikha02@gmail.com>
2023-06-23 09:14:05 +0530

    features:cart class and repository  created

---
+++ backend/src/main/java/com/moshop/backend/model/Cart.java
@@ -0,0 +1,27 @@
+package com.moshop.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Document
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+    @Id
+   private String cartId;
+  private   String customerId;
+   private String productId;
+   private LocalDateTime createdDate;
+    private  boolean isActive;
+
+
+}
---
+++ backend/src/main/java/com/moshop/backend/repository/CartRepository.java
@@ -0,0 +1,10 @@
+package com.moshop.backend.repository;
+
+import com.moshop.backend.model.Cart;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+public interface CartRepository extends MongoRepository<Cart, String> {
+}
