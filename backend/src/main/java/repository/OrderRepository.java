10be4de3f10bd678b805c5181564e0e158678c26
Rashmi <rashmiranjan491@gmail.com>
2023-06-23 09:13:59 +0530

    feature: added order class and OrderRepository

---
+++ backend/src/main/java/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface OrderRepository extends MongoRepository<Order,String> {
+}
