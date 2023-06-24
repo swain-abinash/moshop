8f84c561ea76461cf8de84a4b9214b89826096eb
Rashmi <rashmiranjan491@gmail.com>
2023-06-24 21:25:47 +0530

    created the ordercontroller class

---
+++ backend/src/main/java/com/moshop/backend/controller/OrderController.java
@@ -0,0 +1,66 @@
+package com.moshop.backend.controller;
+
+import com.moshop.backend.model.Order;
+import com.moshop.backend.services.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping("/{customerId}")
+    public ResponseEntity<Void> createOrder(@PathVariable String customerId, @RequestBody Order order){
+        orderService.createOrder(customerId, order);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Order>> getOrders(){
+        List<Order> orders = orderService.getOrders();
+        return ResponseEntity.ok(orders);
+    }
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<List<Order>> getOrdersByCustomerId(@PathVariable String customerId){
+        List<Order> orders=orderService.getOrders(customerId);
+        return ResponseEntity.ok(orders);
+    }
+
+    @GetMapping("/{orderId}/{customerId}")
+    public ResponseEntity<Order> getOrder(@PathVariable String orderId, @PathVariable String customerId){
+        Order order=orderService.getOrder(orderId, customerId);
+        if (order != null){
+            return ResponseEntity.ok(order);
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Order> getOrderById(@PathVariable String orderId){
+        Order order=orderService.getOrder(orderId);
+        if (order != null){
+            return ResponseEntity.ok(order);
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/count")
+    public  ResponseEntity<Long> countAllOrders(){
+        long count=orderService.countAll();
+        return ResponseEntity.ok(count);
+    }
+
+    @GetMapping("/count/{customerId}")
+    public ResponseEntity<Long> countOrdersByCustomerId(@PathVariable String customerId){
+        long count=orderService.countAll(customerId);
+        return ResponseEntity.ok(count);
+    }
+}
