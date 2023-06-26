02966d9f22a1c6ed2a1f2e1e09615c5e46de1cb3
Barsha Rana <ranadebasmita1999@gmail.com>
2023-06-26 23:05:44 +0530

    feature: VariantRequestDTO and VariantResponseDTO add

---
+++ backend/src/main/java/com/moshop/backend/model/entity/dto/VariantResponseDTO.java
@@ -0,0 +1,7 @@
+package com.moshop.backend.model.entity.dto;
+
+public class VariantResponseDTO {
+    private String variantName;
+    private String variantId;
+    private String variantDescrption;
+}
