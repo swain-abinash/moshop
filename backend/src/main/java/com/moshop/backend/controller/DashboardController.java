ed2c92c2915d4506aa9b3d45d23add9d6ce820b1
Sagar-Mallik <mrsagarxt@gmail.com>
2023-06-27 07:55:12 +0530

    Changes cunstroctor from repository to service

--- backend/src/main/java/com/moshop/backend/controller/DashboardController.java
+++ backend/src/main/java/com/moshop/backend/controller/DashboardController.java
@@ -1,6 +1,7 @@
 package com.moshop.backend.controller;
 
 import com.moshop.backend.model.entity.Dashboard;
+import com.moshop.backend.services.*;
 import com.moshop.backend.services.impl.DashboardServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,11 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/dashboard")
 @RequiredArgsConstructor
 public class DashboardController {
-    private final DashboardServiceImpl dashboardServiceImpl;
+    private final VariantService variantService;
+    private final ProductService productService;
+    private final CustomerService customerService;
+    private final CartService cartService;
+    private final OrderService orderService;
 
     @GetMapping
     public ResponseEntity<Dashboard> countAll() {
-        var dashboard = dashboardServiceImpl.countAll();
+        var dashboard = new Dashboard();
+
+        dashboard.setTotalVariants(variantService.countAll());
+        dashboard.setTotalProducts(productService.countAll());
+        dashboard.setTotalCustomers(customerService.countAll());
+        dashboard.setTotalCarts(cartService.countAll());
+        dashboard.setTotalOrders(orderService.countAll());
+
         return ResponseEntity.status(HttpStatus.OK).body(dashboard);
     }
 }
--- backend/src/main/java/com/moshop/backend/services/impl/DashboardServiceImpl.java
+++ backend/src/main/java/com/moshop/backend/services/impl/DashboardServiceImpl.java
@@ -9,22 +9,10 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class DashboardServiceImpl implements DashboardService {
-    private final VariantRepository variantRepository;
-    private final ProductRepository productRepository;
-    private final CustomerRepository customerRepository;
-    private final CartRepository cartRepository;
-    private final OrderRepository orderRepository;
 
     @Override
     public Dashboard countAll() {
         var dashboard = new Dashboard();
-
-        dashboard.setTotalVariants(variantRepository.count());
-        dashboard.setTotalProducts(productRepository.count());
-        dashboard.setTotalCustomers(customerRepository.count());
-        dashboard.setTotalCarts(cartRepository.count());
-        dashboard.setTotalOrders(orderRepository.count());
-
         return dashboard;
     }
 }
